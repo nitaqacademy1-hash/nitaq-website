8d6f47f324b3e5241cfe18301a2dc61eec45dde3
alifglobalschool321 <midlajmidu910@gmail.com>
2026-05-20 16:20:10 +0530

    fix(ssr): revert StaticRouter import and fix build

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,23 +1,19 @@
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
-import { StrictMode } from 'react'
 import App from './App.jsx'
 
 export function render(url, context = {}) {
   const helmetContext = {}
   
   const html = renderToString(
-    <StrictMode>
-      <HelmetProvider context={helmetContext}>
-        <StaticRouter location={url} context={context}>
-          <App />
-        </StaticRouter>
-      </HelmetProvider>
-    </StrictMode>
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </HelmetProvider>
   )
 
-  // Explicitly extract the helmet instance from the completed context object
   const { helmet } = helmetContext
 
   return {
@@ -27,6 +23,6 @@ export function render(url, context = {}) {
       ${helmet.meta ? helmet.meta.toString() : ''}
       ${helmet.link ? helmet.link.toString() : ''}
       ${helmet.script ? helmet.script.toString() : ''}
-    ` : ''
+    `.trim() : ''
   }
 }
