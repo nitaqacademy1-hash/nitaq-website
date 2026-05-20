9cf55cbe7c05d1f86613a3a970366ffe610192cf
alifglobalschool321 <midlajmidu910@gmail.com>
2026-05-20 16:32:08 +0530

    fix(client): align HelmetProvider wrapper in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot, hydrateRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 
-const container = document.getElementById('root')
-
-const app = (
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>
+  </React.StrictMode>
 )
-
-// If the page was pre-rendered (has real content inside #root), hydrate.
-// Otherwise, do a fresh render (dev mode / first load).
-if (container.hasChildNodes()) {
-  hydrateRoot(container, app)
-} else {
-  createRoot(container).render(app)
-}
